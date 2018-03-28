ff344aeb16c7b3152da991a7e5accdf675098176
eyi1 <ej7729@gmail.com>
2018-03-28 01:05:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime? (n)
   return false if n < 2
-     if (2...n).any? {|i| n % i == 0}
-       false
-     else
+     if (2...n).each {|i| return false if n % i == 0}
        true
      end
 end
