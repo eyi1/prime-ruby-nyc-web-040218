48dce3ebcd78dce513b7e10668bb1e79c1aec14a
eyi1 <ej7729@gmail.com>
2018-03-28 00:58:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
-def prime? (integer)
-return false if n < 2
-return true if n == 3 || n == 2 
-  if (2...n-1).any? {|i| n % i == 0}
-    false
-  else
-    true
-  end
+def prime? (n)
+  return false if n < 2 
+     if (2...n-1).any?{|i| n % i == 0}
+       false
+     else
+       true
+     end
 end
 # Add  code here!
